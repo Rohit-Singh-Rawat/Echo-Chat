8a69486b58ab86d8eb42eeac7e7751752750b99d
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-11-11 18:31:37 +0530

    fixes:

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import '@repo/ui/globals.css';
+import { Button } from '@repo/ui/components/ui/button';
 
-import { Button } from '@repo/ui/button';
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -29,7 +29,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Button appName='h'>d</Button>
+				<Button >d</Button>
 				{children}
 			</body>
 		</html>
